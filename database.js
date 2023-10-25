016f208b677629d15f0a96d34f71afac92a9814b
JazzieL Rodríguez López <jazzielrodriguezlopez@gmail.com>
2023-10-24 23:37:59 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,6 +1,6 @@
 const mongoose = require ('mongoose');
 
 
-mongoose.connect('mongodb+srv://JazzoLopez:rVZ29T6UqNBDrCHv@cluster0.oahxtpe.mongodb.net/iotdb?retryWrites=true&w=majority&appName=AtlasApp')
+mongoose.connect('mongodb+srv://<Username>:<Password>@cluster0.oahxtpe.mongodb.net/<dbName?retryWrites=true&w=majority&appName=AtlasApp')
     .then(db => console.log('Mongodb atlas connected')) //Si fue exitoso
     .catch(err => console.error(err));
